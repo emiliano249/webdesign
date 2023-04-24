7299c48bbd5d87b4083393a6c61647541e2794e7
Emiliano Esteban <50309405+emiliano249@users.noreply.github.com>
2023-04-24 16:25:44 -0300

    Add files via upload

---
+++ assetsContadorEmi/js/custom.js
@@ -0,0 +1,17 @@
+(function ($) {
+	
+	
+
+
+	// Home number counterup codigo mas importante EMI
+	if($('.count-item').length){
+		$('.count-item strong').counterUp({
+			delay: 10,
+			time: 1000
+		});
+	}
+
+
+	
+
+})(window.jQuery);
